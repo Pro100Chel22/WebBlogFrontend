2c5e26a731ccf861117474f60e4084cb56daef53
Vitaliy Ivanov <ivanoff4123@gmail.com>
2023-11-18 00:30:11 +0700

    update navbar and change Rounter
    Добавил для navbar отображене почты, если он авторзирован. Перенес Router в отдельный файл.

--- js/index.js
+++ js/index.js
@@ -1,35 +1,4 @@
-let Router = {
-    routes: {
-        "/login": "/modules/login.html",
-        "/registration": "/modules/registration.html"
-    },
-
-    init: function() {
-        this._routes = [];
-
-        for(let route in this.routes) {
-            this._routes.push({
-                pattern: new RegExp('^' + route.replace(/:\w+/g,'(\\w+)') + '$'),
-
-                path: this.routes[route]
-            });
-        }
-    },
-
-    dispatch: function(path) {
-        var i = this._routes.length;
-       
-        while(i--) {
-            var args = path.match(this._routes[i].pattern);
-           
-            if(args) {
-                return this._routes[i];
-            }
-        }
-
-        return false;
-    },
-}
+import { Router } from './router.js';
 
 function includeHTML(file) {
     var xhr = new XMLHttpRequest();
@@ -45,13 +14,48 @@ function includeHTML(file) {
     xhr.send();
 }
 
-Router.init();
-
-let filePath = Router.dispatch(window.location.pathname);
-if(filePath) {
-    includeHTML(filePath.path);
+let route = Router.findFilePath(window.location.pathname);
+if(route) {
+    includeHTML(route.filePath);
 } 
 else {
     includeHTML("/modules/error.html");
+}
+
+localStorage.setItem('JWTToken', "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1laWQiOiI1ZTY4Njg4Yi00ZmU3LTQ1NjQtNmY5ZC0wOGRiZTc4ZjAyYmIiLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9hdXRoZW50aWNhdGlvbiI6ImEwN2Q3MGRhLWEwMjctNGI1OC1iMWU3LTMwMDI3MTVlNDE2NCIsIm5iZiI6MTcwMDI0MDU2MSwiZXhwIjoxNzAwMjQ0MTYxLCJpYXQiOjE3MDAyNDA1NjEsImlzcyI6IkJsb2cuQVBJIiwiYXVkIjoiQmxvZy5BUEkifQ.6TbnBOYR8LmYtNd9LhJTP1q4UoMg8jCLDO14cyII75g");
+const userToken = localStorage.getItem('JWTToken');
+
+if (userToken !== null) {
+  console.log('Токен пользователя существует:', userToken);
+  
+  fetch('https://blog.kreosoft.space/api/account/profile', {
+        method: 'GET',
+        headers: {
+            'accept': 'text/plain',
+            'Authorization': "Bearer " + userToken
+    }
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+
+        return response.json();
+    })
+    .then(data => {
+        console.log(data); 
+
+        document.getElementById('login_id').classList.add('d-none');
+        document.getElementById('placeholder_user_email_id').classList.remove('d-none');
+
+        document.getElementById('user_email_id').innerHTML = data.email;
+    })
+    .catch(error => {
+        console.error('There has been a problem with your fetch operation:', error);
+    });
+
+} else {
+    document.getElementById('login_id').classList.remove('d-none');
+    document.getElementById('placeholder_user_email_id').classList.add('d-none');
 }
 
---
+++ js/router.js
@@ -0,0 +1,36 @@
+export { Router }
+
+let Router = {
+    routes: {
+        "/login": "/modules/login.html",
+        "/registration": "/modules/registration.html"
+    },
+
+    init: function() {
+        this._routes = [];
+
+        for(let route in this.routes) {
+            this._routes.push({
+                pattern: new RegExp('^' + route.replace(/:\w+/g,'(\\w+)') + '$'),
+
+                filePath: this.routes[route]
+            });
+        }
+    },
+
+    findFilePath: function(filePath) {
+        var i = this._routes.length;
+       
+        while(i--) {
+            var args = filePath.match(this._routes[i].pattern);
+           
+            if(args) {
+                return this._routes[i];
+            }
+        }
+
+        return false;
+    },
+}
+
+Router.init();
