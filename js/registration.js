207c5a624ccc0f07325ac8b688f4b4a56809ae74
Vitaliy Ivanov <ivanoff4123@gmail.com>
2023-11-22 02:39:34 +0700

    add email server validate

--- js/registration.js
+++ js/registration.js
@@ -2,21 +2,11 @@ import { loadPageWithoutReload, saveInitFuncAndRun } from './tools/loadMainConte
 import { userIsAuthorized, userIsNotAuthorized } from './index.js';
 import { request } from './tools/request.js';
 import IMask from '../node_modules/imask/esm/index.js';
-//import 'air-datepicker';
-//import AirDatepicker from 'air-datepicker'
-import AirDatepicker from '../node_modules/air-datepicker/index.es.js';
-//import '../node_modules/air-datepicker/air-datepicker.css';
 
 function init() {
     new IMask(document.getElementById('phone_input_id'), {
         mask: "+{7} (000) 000-00-00"
     });
-
-    // $('#birthd_input_id').datepicker({
-    //     // настройки
-    //   });
-
-    //new AirDatepicker('#birthd_input_id');
 
     const formsId = ['#name_input_id', '#birthd_input_id', '#phone_input_id', '#gender_input_id', '#email_input_id', '#password_input_id'];
 
@@ -27,21 +17,25 @@ function init() {
         });
     });
 
-    $('#birthd_input_id').on('keydown input', function() {
-        let birthd = $(this).val();
-        if (birthd.length !== 0) {
-            let parent = $(this).parent();
-            parent.removeClass('was-validated');
+    $('#birthd_input_id').on('input', function() {
+        const userDate = new Date($('#birthd_input_id').val());
+        const minDate = new Date('01.01.1901');
+        const maxDate = new Date();
 
-            // $(this).addClass('is-invalid');
-            // $(this).removeClass('is-valid');
+        if (userDate > maxDate) {
+            const formattedCurrentDate = `${maxDate.getFullYear()}-${String(maxDate.getMonth() + 1).padStart(2, '0')}-${String(maxDate.getDate()).padStart(2, '0')}`;
+
+            $('#birthd_input_id').val(formattedCurrentDate);
+            
+        } else if (userDate < minDate) {
+            $('#birthd_input_id').val('1901-01-01');
         }
-        else {
-            let parent = $(this).parent();
-            parent.addClass('was-validated');
-            $(this).removeClass('is-invalid');
-            $(this).removeClass('is-valid');
-        }
+    });
+
+    $('#email_input_id').on('focus', function() {
+        $('#email_input_id').removeClass('is-invalid');
+        $('#email_incorrect_id').removeClass('d-none');
+        $('#email_exist_id').addClass('d-none');
     });
 
     const forms = document.querySelectorAll('.needs-validation');
@@ -50,112 +44,62 @@ function init() {
             event.preventDefault();
             event.stopPropagation();
 
+            $('#registration_form_id :input').prop('disabled', true);
+
             Array.from(formsId).forEach(inputs => {
                 $(inputs).parent().addClass('was-validated');
+                $(inputs).removeClass('is-invalid');
             });
-
-            // console.log($(birthd_input_id).val().length === 0);
-
-            const login = (data) => {
-                console.log(data);
-                if (data.status === 200) {
-                    localStorage.setItem('JWTToken', data.body.token);
-                    userIsAuthorized($('#email_input_id').val());
-                }
-                else {
-                    userIsNotAuthorized();
-                }
-            }
 
             const body = {
                 "fullName": $('#name_input_id').val(),
                 "password": $('#password_input_id').val(),
                 "email": $('#email_input_id').val(),
-                "birthDate": null,
+                "birthDate": $('#birthd_input_id').val().length !== 0 ? dateConvertToUTCWithSmooth($('#birthd_input_id').val(), 1) : null,
                 "gender": $('#gender_input_id').val(),
                 "phoneNumber": $('#phone_input_id').val().length !== 0 ? $('#phone_input_id').val() : null
             }
-    
+
             request('https://blog.kreosoft.space/api/account/register', 'POST', login, body);
             
         }, false)
     });
 }
 
+function login (data) {
+    console.log(data);
+    if (data.status === 200) {
+        localStorage.setItem('JWTToken', data.body.token);
+        userIsAuthorized($('#email_input_id').val());
+
+        //loadPageWithoutReload("/");
+        return;
+    }
+    else if (data.status === 400) {
+        if (data.body?.DuplicateUserName ?? false) {
+            console.log(data.body.DuplicateUserName);
+
+            let parent = $('#email_input_id').parent();
+            parent.removeClass('was-validated');
+
+            $('#email_input_id').addClass('is-invalid');
+            $('#email_incorrect_id').addClass('d-none');
+            $('#email_exist_id').removeClass('d-none');
+
+            $('#registration_form_id :input').prop('disabled', false);
+        }
+    }
+
+    userIsNotAuthorized();
+}
+
+function dateConvertToUTCWithSmooth(dateStr, dH = 0, dM = 0, dS = 0) {
+    const date = new Date(dateStr);
+
+    const currentDate = new Date(); 
+    date.setHours(currentDate.getHours() - dH, currentDate.getMinutes() - dM, currentDate.getSeconds() - dS);
+
+    return date.toISOString();
+}
+
 saveInitFuncAndRun(init);
-
-// повесть на Input слушатель и проверять корректность
-// сохранить эти валидации
-// когда нажал на дату она стала was-validted
-// когда нажали на submit идет проверка всех валидаци
-// если пришел отват, что неправльная дата снять с нее was-validted и поставиить is-invalid, при фокусировки снять is-invalid и поставить was-validted
-
-// $('#birthd_input_id').on('input invalid keydown', function(event) {
-        
-        
-        
-    //     var inputValue = $(this).val();
-    //     console.log(inputValue);
-
-
-    
-    //     var currentDate = event.target.value;
-    //     console.log("Текущая дата:", event);
-    //     // if(inputValue.length < 3) {
-    //     //     $(this).addClass('is-invalid');
-    //     //     $(this).removeClass('is-valid');
-    //     // } 
-    //     // else {
-    //     //     $(this).removeClass('is-invalid');
-    //     //     $(this).addClass('is-valid');
-    //     // }
-    // });
-
-
-    //console.log(form);
-
-            // if (form.checkValidity()) {
-            //     // var passwordValue = $('#password_input_id').val();
-
-            //     // if(passwordValue !== "123456") {
-            //     //     $('#password_input_id').addClass('is-invalid');
-            //     // }
-
-            //     //form.classList.remove('was-validated');
-            // }
-            // else {
-            //     console.log()
-            //     //form.classList.add('was-validated');
-            // }
-
-
-
-    // $('#name_input_id').on('input focus', function() {
-    //     var parent = $(this).parent();
-    //     parent.addClass('was-validated');
-    // });
- 
-    // $('#birthd_input_id').on('focus', function() {
-    //     var parent = $(this).parent();
-    //     parent.addClass('was-validated');
-    // });
-
-    // $('#phone_input_id').on('focus', function() {
-    //     var parent = $(this).parent();
-    //     parent.addClass('was-validated');
-    // });
-
-    // $('#gender_input_id').on('focus', function() {
-    //     var parent = $(this).parent();
-    //     parent.addClass('was-validated');
-    // });
-    
-    // $('#email_input_id').on('focus', function() {
-    //     var parent = $(this).parent();
-    //     parent.addClass('was-validated');
-    // });
-    
-    // $('#password_input_id').on('focus', function() {
-    //     var parent = $(this).parent();
-    //     parent.addClass('was-validated');
-    // });
